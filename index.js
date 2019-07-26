4947d4875d58c7addeec08c6707eb7454844d8cf
Kevin Millar <koden.km@gmail.com>
2019-07-26 13:59:04 +1000

    Remove unneeded meta tag and service worker logging.

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 'use strict';
 
-let serviceWorkerRegistration;
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('sw.js')
-		.then(function (registration) {
-			serviceWorkerRegistration = registration;
-			console.log('PWA: Service Worker Registered');
-		});
-
-	navigator.serviceWorker.ready
-		.then(function (registration) {
-			console.log('PWA: Service Worker Ready');
-		});
+	navigator.serviceWorker.register('sw.js');
 }
